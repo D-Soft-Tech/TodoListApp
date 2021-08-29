7c5baa9e26a19315fa2f206b2d489d9336bc8366
DSoftTech <81322397+DSoftTech@users.noreply.github.com>
2021-08-29 16:49:56 +0100

    Fixed the dependency error by removing the safeargs dependency that was throwing the error

--- app/src/main/java/com/example/mvvmtodolistapp/Tasks.kt
+++ app/src/main/java/com/example/mvvmtodolistapp/Tasks.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mvvmtodolistapp.databinding.FragmentTasksBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Tasks : Fragment() {
     private var _binding: FragmentTasksBinding? = null
     private val binding get() = _binding!!
+    private lateinit var addFab: FloatingActionButton
+    private lateinit var tasksRV: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,7 +24,14 @@ class Tasks : Fragment() {
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        // View initialization
+        addFab = binding.addTaskBtn
+        tasksRV = binding.tasksRv
 
+        // Set OnClick Listener on the fab to navigate to the add screen
+        addFab.setOnClickListener {
+//            findNavController().navigate()
+        }
 
         return view
     }
